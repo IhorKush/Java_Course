a9ed200a715030550b6fea45ab05ba397a68141c
Ihor Yakushev <ihor.yakushev33@mail.com>
2024-10-08 21:18:40 +0300

    Add comments

---
+++ src/syntax/lesson2_comments/Main.java
@@ -0,0 +1,13 @@
+package syntax.lesson2_comments;
+
+/*
+* @author Ihor Kush
+* @version 1.0.2024
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        //Print my name...
+        System.out.println("My name is Ihor Kush");
+    }
+}
