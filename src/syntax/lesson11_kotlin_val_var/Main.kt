88f0275ced9d57c64f87763ed4fac06299f33b26
Ihor Yakushev <ihor.yakushev33@mail.com>
2024-10-08 22:07:30 +0300

    Kotlin_val_var

---
+++ src/syntax/lesson11_kotlin_val_var/Main.kt
@@ -0,0 +1,15 @@
+package syntax.lesson11_kotlin_val_var
+
+fun main() {
+    //Java
+    //int nameOfVariable=5;
+
+
+    //kotlin
+    var intNumber: Int = 5
+    var someString: String = "My name is Ihor Yakushev!"
+
+    intNumber = 4
+
+    val nameOfManager = "Ihor Yakushev"
+}
