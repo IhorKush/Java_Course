87ad54e292225667acc75a487abae6b1f5a6518b
Ihor Yakushev <ihor.yakushev33@mail.com>
2024-10-08 22:41:55 +0300

    Null_Demo

---
+++ src/syntax/lesson13_null/NullDemo.java
@@ -0,0 +1,12 @@
+package syntax.lesson13_null;
+
+public class NullDemo {
+    public static void main(String[] args) {
+
+        String nullableString = null;
+        nullableString.length();
+        System.out.println(nullableString);
+        Integer nullableInt = null;
+        System.out.println(nullableInt);
+    }
+}
