f2d653b35d89a1baf31412285c49da67cf512731
Ihor Yakushev <ihor.yakushev33@mail.com>
2024-10-08 22:12:24 +0300

    Kotlin_templates

---
+++ src/syntax/lesson12_String_templates/Main.kt
@@ -0,0 +1,13 @@
+package syntax.lesson12_String_templates
+
+fun main() {
+
+    val nameOfManager = "Ihor Yakushev"
+
+    println("Hello, $nameOfManager !")
+
+    var a = 4
+    var b = 8
+
+    println("The sum a + b = ${a + b}")
+}
