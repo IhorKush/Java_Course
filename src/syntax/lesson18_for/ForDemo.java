94d1f234364e85069a01d4a51f27ba716e16877b
Ihor Yakushev <ihor.yakushev33@mail.com>
2024-10-10 21:41:52 +0300

    For_Demo

---
+++ src/syntax/lesson18_for/ForDemo.java
@@ -0,0 +1,12 @@
+package syntax.lesson18_for;
+
+public class ForDemo {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 0 to 10 is " + sum);
+    }
+}
